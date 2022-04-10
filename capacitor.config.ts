d2e9e66dcda88c615fd4af041cf0162102ac440b
LOTRYOL <amadordejuegos@gmail.com>
2022-04-09 20:41:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'M&S',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
